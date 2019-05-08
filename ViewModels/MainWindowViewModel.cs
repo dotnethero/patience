a5fbb34d36f3c0eb757c36912769079bc30eb95a
Andrew Gorbokon <andrew.gorbokon@gmail.com>
2019-05-09 00:32:41 +0300

    Add project files.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patience.ViewModels
+{
+    internal class MainWindowViewModel
+    {
+        public string File1 { get; set; }
+        public string File2 { get; set; }
+
+        public MainWindowViewModel()
+        {
+            File1 = File.ReadAllText("Data/413158_source.txt");
+            File2 = File.ReadAllText("Data/413158_recalc.txt");
+        }
+    }
+}
---
+++ Views/BindableRichTextBox.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Patience.Views
+{
+    /// <summary>
+    /// Interaction logic for BindableRichTextBox.xaml
+    /// </summary>
+    public partial class BindableRichTextBox : UserControl
+    {
+        public BindableRichTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue is string text)
+            {
+                var doc = new FlowDocument();
+                doc.PageWidth = 1000; // fast resize tweak
+                var lines = text.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                foreach (var line in lines)
+                {
+                    var para = new Paragraph();
+                    para.Inlines.Add(line);
+                    doc.Blocks.Add(para);
+                }
+                textBox.Document = doc;
+            }
+        }
+
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+        }
+    }
+}
